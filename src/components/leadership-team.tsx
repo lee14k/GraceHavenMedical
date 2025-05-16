af849583e27d75d30b842900735d62c9fcb024a8
lee14k <kmkovach@mtu.edu>
2025-05-16 02:52:16 -0500

    updating leadership team

--- src/components/leadership-team.tsx
+++ src/components/leadership-team.tsx
@@ -23,13 +23,15 @@ export default function LeadershipTeam(props: LeadershipTeamProps) {
     <Container fluid style={{ color: '#aa9c91', backgroundColor: '#ffffff' }}>
       <Container className="pt-4 pb-4">
         <h2>{props.name}</h2>
-        <Row className="mt-4">
+        <Row className="mt-4 justify-content-center">
           {props.items.map((item) => (
-            <Col md="3" className="mt-4 mb-4" key={`${item.name}`}>
-              <Image className={styles.leadershipTeamImage} src={item.imageURI} alt={`${item.name} image`} width={296} height={355} />
-              <strong>{item.name.toUpperCase()}</strong>
-              <p>{item.title}</p>
-              <p>{item.text}</p>
+            <Col xs="12" md="6" lg="5" className="mt-4 mb-4" key={`${item.name}`}>
+              <div className="d-flex flex-column align-items-center">
+                <Image className={styles.leadershipTeamImage} src={item.imageURI} alt={`${item.name} image`} width={296} height={355} />
+                <strong className="text-center mt-3">{item.name.toUpperCase()}</strong>
+                <p className="text-center">{item.title}</p>
+                <p className="text-center">{item.text}</p>
+              </div>
             </Col>
           ))}
         </Row>
